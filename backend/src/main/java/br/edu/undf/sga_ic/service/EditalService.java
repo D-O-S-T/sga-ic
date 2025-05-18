7f3cae80c520115b68be4510644810400f8b44aa
natanDini <natangsdiniz@gmail.com>
2025-05-18 18:33:46 -0300

    Creating logic for listing notices

--- backend/src/main/java/br/edu/undf/sga_ic/service/EditalService.java
+++ backend/src/main/java/br/edu/undf/sga_ic/service/EditalService.java
@@ -1,13 +1,20 @@
 package br.edu.undf.sga_ic.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.undf.sga_ic.dto.req.EditalAdd;
+import br.edu.undf.sga_ic.dto.res.EditalRes;
+import br.edu.undf.sga_ic.dto.res.EditalResShort;
 import br.edu.undf.sga_ic.dto.res.Retorno;
+import br.edu.undf.sga_ic.exception.CustomException;
 import br.edu.undf.sga_ic.model.Edital;
 import br.edu.undf.sga_ic.repository.EditalRepository;
+import br.edu.undf.sga_ic.utils.EditalUtils;
+import br.edu.undf.sga_ic.utils.EmptyUtils;
 import br.edu.undf.sga_ic.utils.RetornoUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +24,8 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class EditalService {
 
+	private final EmptyUtils emptyUtils;
+	private final EditalUtils editalUtils;
 	private final RetornoUtils retornoUtils;
 
 	private final EditalRepository editalRepository;
@@ -40,5 +49,37 @@ public class EditalService {
 
 		log.info("Edital registrado com sucesso.");
 		return retornoUtils.retornoSucesso("Edital registrado com sucesso.");
+	}
+
+	public List<EditalResShort> findAll() throws CustomException {
+
+		List<Edital> editais = editalRepository.findAll();
+
+		emptyUtils.validaListaVazia(editais, "Não foram encontrados editais registrados.");
+
+		log.info(">>> Retornando lista de editais com sucesso.");
+		return mapEditaisToDTO(editais);
+	}
+
+	public EditalRes findById(Long editalId) throws CustomException {
+
+		Edital edital = editalUtils.findById(editalId);
+
+		EditalRes editalDTO = EditalRes.builder().id(edital.getId()).titulo(edital.getTitulo())
+				.instituicao(edital.getInstituicao()).descricao(edital.getDescricao())
+				.qtdBolsistas(edital.getQtdBolsistas()).qtdAlunos(edital.getQtdAlunos())
+				.qtdProfessores(edital.getQtdProfessores()).qtdProjetos(edital.getQtdProjetos())
+				.dtInicio(edital.getDtInicio()).dtFim(edital.getDtFim()).build();
+
+		log.info(">>> Retornando edital pelo id com sucesso.");
+		return editalDTO;
+	}
+
+	private List<EditalResShort> mapEditaisToDTO(List<Edital> editais) {
+		return editais.stream()
+				.map(edital -> EditalResShort.builder().id(edital.getId()).titulo(edital.getTitulo())
+						.instituicao(edital.getInstituicao()).dtInicio(edital.getDtInicio()).dtFim(edital.getDtFim())
+						.build())
+				.toList();
 	}
 }
--- backend/src/main/java/br/edu/undf/sga_ic/service/ProjetoService.java
+++ backend/src/main/java/br/edu/undf/sga_ic/service/ProjetoService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.undf.sga_ic.dto.req.ProjetoAdd;
-import br.edu.undf.sga_ic.dto.res.EditalResShort;
 import br.edu.undf.sga_ic.dto.res.ProjetoRes;
 import br.edu.undf.sga_ic.dto.res.Retorno;
 import br.edu.undf.sga_ic.exception.CustomException;
@@ -68,10 +67,7 @@ public class ProjetoService {
 		Projeto projeto = projetoUtils.findById(projetoId);
 
 		ProjetoRes projetoDTO = ProjetoRes.builder().id(projeto.getId()).titulo(projeto.getTitulo())
-				.descricao(projeto.getDescricao())
-				.edital(EditalResShort.builder().titulo(projeto.getEdital().getTitulo())
-						.instituicao(projeto.getEdital().getInstituicao()).build())
-				.build();
+				.descricao(projeto.getDescricao()).build();
 
 		log.info(">>> Retornando projeto de Id: {} - com sucesso.", projeto.getId());
 		return projetoDTO;
@@ -104,13 +100,8 @@ public class ProjetoService {
 	}
 
 	private List<ProjetoRes> mapProjetosToDTO(List<Projeto> projetos) {
-		return projetos.stream()
-				.map(projeto -> ProjetoRes.builder().id(projeto.getId()).titulo(projeto.getTitulo())
-						.descricao(projeto.getDescricao())
-						.edital(EditalResShort.builder().titulo(projeto.getEdital().getTitulo())
-								.instituicao(projeto.getEdital().getInstituicao()).build())
-						.build())
-				.toList();
+		return projetos.stream().map(projeto -> ProjetoRes.builder().id(projeto.getId()).titulo(projeto.getTitulo())
+				.descricao(projeto.getDescricao()).build()).toList();
 	}
 
 	public void validaMaxProjetosByEdital(Edital edital) throws CustomException {
