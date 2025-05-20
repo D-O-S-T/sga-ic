de1eedae04f00ebd174a33da199d820b27aeff81
alicesantosc <alice.santos@undf.edu.br>
2025-05-19 22:58:01 -0300

    feature: edital form styles and implementation

--- frontend/src/app/pages/coordenador/formulario-edital/formulario-edital.component.ts
+++ frontend/src/app/pages/coordenador/formulario-edital/formulario-edital.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Optional } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, Optional } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,36 +6,53 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core'; 
+import { EditalService } from '../../../services/edital.service';
+import { Edital } from '../../../services/edital.service';
 
 @Component({
   selector: 'app-formulario-edital',
   templateUrl: './formulario-edital.component.html',
   styleUrls: ['./formulario-edital.component.scss'],
   standalone: true,
-  // changeDetection: ChangeDetectionStrategy.OnPush, // opcional
+  changeDetection: ChangeDetectionStrategy.OnPush, 
   imports: [
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
     MatDialogModule,
-    FormsModule,
+    FormsModule,MatDatepickerModule,
+    MatNativeDateModule
   ],
 })
 export class FormularioEditalComponent {
   edital = {
     titulo: '',
     descricao: '',
-    dataInicio: '',
-    dataFim: ''
+    dtInicio: '',
+    dtFim: '',
+    instituicao: '',
+    qtdAlunos: 0,
+    qtdProjetos: 0,
+    qtdBolsistas: 0,
+    qtdProfessores: 0
   };
 
    constructor(
+      private editalService: EditalService,
     @Optional() @Inject(MatDialogRef) public dialogRef: MatDialogRef<FormularioEditalComponent>
   ) { }
   salvar() {
-    this.dialogRef.close(this.edital);
-  }
+      this.editalService.criarEdital(this.edital).subscribe({
+     next: (response: Edital) => {
+  console.log('Edital salvo com sucesso!', response);
+  this.dialogRef.close(response); // Ou this.edital se preferir
+}
+ });
+
+  } 
 
   fechar() {
     this.dialogRef.close();
