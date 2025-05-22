919d058eb810ce9ca7eadac23e239421df937d6f
alicesantosc <alice.santos@undf.edu.br>
2025-05-21 22:48:09 -0300

    feature: init professor screen

--- frontend/src/app/pages/coordenador/coordenador.component.ts
+++ frontend/src/app/pages/coordenador/coordenador.component.ts
@@ -1,36 +1,21 @@
 
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import { MatDialog } from '@angular/material/dialog';
-import {MatDialogRef} from '@angular/material/dialog';
-import {FormularioEditalComponent} from './formulario-edital/formulario-edital.component';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { SidebarComponent, NavItem  } from '../../shared/sidebar/sidebar.component';
+
 
 @Component({
   selector: 'app-coordenador',
   standalone: true,
-   imports: [MatCardModule, MatButtonModule,MatListModule, MatDividerModule, FormularioEditalComponent,  MatDatepickerModule,
-  MatNativeDateModule ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+   imports: [HeaderComponent, SidebarComponent],
   templateUrl: './coordenador.component.html',
   styleUrl: './coordenador.component.scss'
 })
 export class CoordenadorComponent {
 
-   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`;
-
-   constructor(private dialog: MatDialog) {}
-
-  abrirFormulario() {
-    this.dialog.open(FormularioEditalComponent, {
-      width: '90%' // pode ajustar o tamanho
-    });
-  }
+   navItems: NavItem[] = [
+      { label: 'Dashboard', route: '/listar-alunos' },
+      { label: 'Usuários', route: '/usuarios' },
+    ];
 
 }
--- frontend/src/app/pages/professor/professor.component.ts
+++ frontend/src/app/pages/professor/professor.component.ts
@@ -1,12 +1,23 @@
+
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { SidebarComponent, NavItem  } from '../../shared/sidebar/sidebar.component';
+
+
 
 @Component({
   selector: 'app-professor',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, SidebarComponent],
   templateUrl: './professor.component.html',
   styleUrl: './professor.component.scss'
 })
 export class ProfessorComponent {
+
+  
+   navItems: NavItem[] = [
+      { label: 'Dashboard', route: '/listar-alunos' },
+      { label: 'Usuários', route: '/usuarios' },
+    ];
 
 }
